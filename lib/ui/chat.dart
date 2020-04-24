dfeccd036816786e29213fa197e4a19b0472d527
PeterCcT <pedrorodriguesfereira@gmail.com>
2020-04-24 17:42:55 -0300

    Construção de mensagens na tela principal

--- lib/ui/chat.dart
+++ lib/ui/chat.dart
@@ -35,7 +35,39 @@ class _ChatState extends State<Chat> {
         title: Text('Teste'),
         elevation: 0,
       ),
-      body: TextComposer(_sendMessage),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance.collection('Mensagens').snapshots(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    List<DocumentSnapshot> docs =
+                        snapshot.data.documents.reversed.toList();
+                    return ListView.builder(
+                      itemCount: docs.length,
+                      reverse: false,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(
+                            docs[index].data['Texto'],
+                          ),
+                        );
+                      },
+                    );
+                }
+              },
+            ),
+          ),
+          TextComposer(_sendMessage),
+        ],
+      ),
     );
   }
 }
