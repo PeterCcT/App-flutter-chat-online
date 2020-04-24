8f230f8755cd34d241c68ca3a6704cdaa2e2383d
PeterCcT <pedrorodriguesfereira@gmail.com>
2020-04-24 15:47:15 -0300

    Configurações iniciais integradas ao firebase

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+
+/*   Firestore.instance
+      .collection('col')
+      .document('doc')
+      .setData({'Text': 'teste'}); */
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
